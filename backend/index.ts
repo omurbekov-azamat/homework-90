54dd769ae96bc67b0c9409a270407b0e5531e760
omurbekov-azamat <avgusttin8@gmail.com>
2023-03-12 18:08:19 +0600

    created WebSocket connection with getting message and sending message back for all;

--- backend/index.ts
+++ backend/index.ts
@@ -1,1 +1,49 @@
-console.log('hello ws')
+import express from 'express';
+import expressWs from "express-ws";
+import cors from 'cors';
+import * as crypto from "crypto";
+import {ActiveConnections, IncomingDraw} from "./types";
+
+const port = 8000;
+const app = express();
+expressWs(app);
+app.use(cors());
+
+const router = express.Router();
+
+const activeConnections: ActiveConnections = {};
+
+router.ws('/canvas', (ws, req) => {
+    const id = crypto.randomUUID();
+    console.log('Client connected! id=', id);
+    activeConnections[id] = ws;
+
+    ws.on('message', (draw) => {
+        const decodeMessage = JSON.parse(draw.toString()) as IncomingDraw;
+
+        switch (decodeMessage.type) {
+            case 'SEND_DRAW':
+                Object.keys(activeConnections).forEach(id => {
+                    const conn = activeConnections[id];
+                    conn.send(JSON.stringify({
+                        type: 'SEND_DRAWS',
+                        payload: decodeMessage.payload,
+                    }))
+                });
+                break;
+            default:
+                console.log('Unknown type', decodeMessage.type);
+        }
+    });
+
+    ws.on('close', () => {
+        console.log('Client disconnected! id=', id);
+        delete activeConnections[id];
+    });
+});
+
+app.use(router);
+
+app.listen(port, () => {
+    console.log('Server started on ', port);
+});
---
+++ backend/types.d.ts
@@ -0,0 +1,13 @@
+import {WebSocket} from 'ws';
+
+export interface ActiveConnections {
+    [id: string]: WebSocket;
+}
+
+export interface IncomingDraw {
+    type: string;
+    payload: {
+        x: number;
+        y: number;
+    };
+}
